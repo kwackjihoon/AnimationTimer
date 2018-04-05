effc391ec665c754767c29a2417d18e95ec365bc
dit <dit@ditui-imac.local>
2018-04-05 14:17:45 +0900

    add repeat

--- AnimationTimer/ViewController.swift
+++ AnimationTimer/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienView: UIImageView!
+    @IBOutlet weak var lblCount: UILabel!
+    var counter = 0
+    var myTimer = Timer()
+    var direction = "right"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblCount.text = String(0)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func play(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.doAnimation), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        myTimer.invalidate()
+        
+    }
+    @objc func doAnimation(){
+        if direction=="right" {
+            if counter==4 {
+                direction = "left"
+            }
+            counter = counter+1
+        }else{
+            if counter==2 {
+                direction = "right"
+            }
+            counter = counter-1
+        }
+        alienView.image = UIImage(named: "frame\(counter).png")
+        lblCount.text = String(counter)
+        
+    }
 
 }
 
